63fc96799d37956fb50a4f7d0501f7f686e9fd37
morenikheji <aishatdosunmu@gmail.com>
2021-04-11 22:36:35 +0100

    task

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const hamburger = document.getElementById("hamburger");
-const navUl = document.querySelector("nav-ul");
-const closeIcon = document.getElementById("close");
-if(window.matchMedia("(max-width: 740px)")){
+const hamburger = document.querySelector("i.hamburger");
+const navUl = document.querySelector("ul.nav-links");
 hamburger.addEventListener("click", ()=>{
-    navUl.classList.add("show");
-    hamburger.style.display="none";
-    closeIcon.classList.replace("close", "close2");
+    navUl.classList.toggle("show");
 })
-closeIcon.addEventListener("click",()=>{
-    navUl.classList.remove("show");
-hamburger.style.display="block";
-closeIcon.classList.replace("close2", "close");
-})}
-else{
-    hamburger.style.display = "none"
-    closeIcon.style.display = "none"
-}
